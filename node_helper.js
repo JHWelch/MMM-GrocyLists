2653bb1e647a28ca891bebecc0f3c061c70f5fd1
Jordan Welch <jwelch@highlandsolutions.com>
2020-08-22 16:45:01 -0500

    Linted

--- node_helper.js
+++ node_helper.js
@@ -5,43 +5,46 @@
  * MIT Licensed.
  */
 
-var NodeHelper = require("node_helper");
+const NodeHelper = require('node_helper');
+
+
+
 
 module.exports = NodeHelper.create({
 
-	// Override socketNotificationReceived method.
+  // Override socketNotificationReceived method.
 
-	/* socketNotificationReceived(notification, payload)
-	 * This method is called when a socket notification arrives.
-	 *
-	 * argument notification string - The identifier of the noitication.
-	 * argument payload mixed - The payload of the notification.
-	 */
-	socketNotificationReceived: function(notification, payload) {
-		if (notification === "MMM-GrocyLists-NOTIFICATION_TEST") {
-			console.log("Working notification system. Notification:", notification, "payload: ", payload);
-			// Send notification
-			this.sendNotificationTest(this.anotherFunction()); //Is possible send objects :)
-		}
-	},
+  /* socketNotificationReceived(notification, payload)
+     * This method is called when a socket notification arrives.
+     *
+     * argument notification string - The identifier of the noitication.
+     * argument payload mixed - The payload of the notification.
+     */
+  socketNotificationReceived(notification, payload) {
+    if (notification === 'MMM-GrocyLists-NOTIFICATION_TEST') {
+      console.log('Working notification system. Notification:', notification, 'payload: ', payload);
+      // Send notification
+      this.sendNotificationTest(this.anotherFunction()); // Is possible send objects :)
+    }
+  },
 
-	// Example function send notification test
-	sendNotificationTest: function(payload) {
-		this.sendSocketNotification("MMM-GrocyLists-NOTIFICATION_TEST", payload);
-	},
+  // Example function send notification test
+  sendNotificationTest(payload) {
+    this.sendSocketNotification('MMM-GrocyLists-NOTIFICATION_TEST', payload);
+  },
 
-	// this you can create extra routes for your module
-	extraRoutes: function() {
-		var self = this;
-		this.expressApp.get("/MMM-GrocyLists/extra_route", function(req, res) {
-			// call another function
-			values = self.anotherFunction();
-			res.send(values);
-		});
-	},
+  // this you can create extra routes for your module
+  extraRoutes() {
+    const self = this;
+    this.expressApp.get('/MMM-GrocyLists/extra_route', (req, res) => {
+      // call another function
+      values = self.anotherFunction();
+      res.send(values);
+    });
+  },
 
-	// Test another function
-	anotherFunction: function() {
-		return {date: new Date()};
-	}
+  // Test another function
+  anotherFunction() {
+    return { date: new Date() };
+  },
 });
